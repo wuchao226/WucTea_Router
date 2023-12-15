2cee62f5303b9015be84b26cda11f9216319601d
wuchao <wuchao5@staff.weibo.com>
2023-12-16 01:02:21 +0800

    [Router] 发布与使用

--- Router/app/src/main/java/com/wuc/router/MainActivity.kt
+++ Router/app/src/main/java/com/wuc/router/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.wuc.router.annotation.Destination
 
-@Destination(url = "router://page-home", description = "应用主页")
+@Destination(url = "router://page-home-test", description = "应用主页测试")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

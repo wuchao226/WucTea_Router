c2f351db236c4471129f7e23f73b7914636195e7
wuchao <wuchao5@staff.weibo.com>
2023-12-19 00:29:17 +0800

    [Router] 自动帮助用户传递路径参数到注解处理器中

--- Router/buildSrc/src/main/groovy/com/wuc/router/gradle/RouterPlugin.groovy
+++ Router/buildSrc/src/main/groovy/com/wuc/router/gradle/RouterPlugin.groovy
@@ -7,6 +7,12 @@ class RouterPlugin implements Plugin<Project> {
   // 实现 apply 方法，注入插件的逻辑
   @Override
   void apply(Project project) {
+    // 1. 自动帮助用户传递路径参数到注解处理器中
+    if (project.extensions.findByName("kapt") != null) {
+      project.extensions.findByName("kapt").arguments {
+        arg("root_project_dir", project.rootProject.projectDir.absolutePath)
+      }
+    }
     println("I am from RouterPlugin, apply from ${project.name}")
     // 2、注册 RouterExtension
     project.getExtensions().create("router", RouterExtension)
